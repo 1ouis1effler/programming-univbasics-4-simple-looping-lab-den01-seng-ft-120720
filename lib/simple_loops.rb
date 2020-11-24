fe03483edb22f80ec0c7c2fbce63c9e67b6d5e69
Louis Leffler <louisleffler@gmail.com>
2020-11-24 20:47:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,14 +1,7 @@
 def loop_message_five_times(message)
   count = 0
   while count < 5 do
-    puts "Hello World"
+    puts message
     count += 1
   end
-end 
-
-def loop_message_n_times(message, limit)
-  count = 15
-  while count < 15 do 
-  puts "Hello Red Ballon"
-  count +=1
 end
