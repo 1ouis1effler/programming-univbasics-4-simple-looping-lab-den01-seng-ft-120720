9e463ac41de080e9ed7971d51c49e96cdf43889c
Louis Leffler <louisleffler@gmail.com>
2020-11-24 20:51:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,8 +24,11 @@ end
 
 def return_string_array(array)
   count = 0
-  while count < array.length do
-    array[count].to_s
-    count += 1
+  new array = []
+  while count <
+  arrary.length do
+    new_array.push(array[count].to_s)
+    count += 1 
   end
+  new_array
 end
